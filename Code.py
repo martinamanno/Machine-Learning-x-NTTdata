de0e478a98138ae1345af31ebea3aefa00b6ff03
MartinaCrisafulli <93279055+MartinaCrisafulli@users.noreply.github.com>
2022-03-25 17:16:44 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+#Import the different datasets 
 geo= pd.read_csv("DATASETS/01.geo.csv",encoding='cp1252',sep=";")
 custom= pd.read_csv("DATASETS/02.customers.csv",encoding='cp1252',sep=";")
 sel= pd.read_csv("DATASETS/03.sellers.csv",encoding='cp1252',sep=";")
@@ -10,6 +11,7 @@ ord_pay= pd.read_csv("DATASETS/06.order_payments.csv",encoding='cp1252',sep=";")
 prod_rev= pd.read_csv("DATASETS/07.product_reviews.csv",encoding='cp1252',sep=";")
 prod= pd.read_csv("DATASETS/08.products.csv",encoding='cp1252',sep=";")
 
+#check for the number of missing values (NaN)
 geo.isna().sum()
 custom.isna().sum()
 sel.isna().sum()
@@ -19,4 +21,13 @@ ord_pay.isna().sum()
 prod_rev.isna().sum()
 prod.isna().sum()
 
+#clean datasets 
 ord_status[["ts_order_delivered_customer","ts_order_delivered_carrier","ts_order_approved","order_id","ts_order_estimated_delivery"]] = ord_status[["ts_order_delivered_customer","ts_order_delivered_carrier","ts_order_approved","order_id","ts_order_estimated_delivery"]].replace(np.nan, "Undefined")
+ord_items["order_id"]= ord_items["order_id"].fillna("Undefined")
+ord_pay["order_id"]= ord_pay["order_id"].fillna("Undefined")
+prod["product_category_name"]= prod["product_category_name"].fillna(mode(prod["product_category_name"]))
+prod[["product_photo_quantity","product_width_cm","product_height_cm",
+      "product_length_cm","product_weight_gr"]] = prod[["product_photo_quantity",
+                                                        "product_width_cm","product_height_cm","product_length_cm",
+                                                        "product_weight_gr"]].replace(np.nan, median(prod[["product_photo_quantity","product_width_cm","product_height_cm","product_length_cm","product_weight_gr"]]))
+                                                                   
