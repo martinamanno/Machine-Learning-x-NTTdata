473c4313dbd153261d650a8aca6fe9429f921816
martinamanno <93279049+martinamanno@users.noreply.github.com>
2022-05-08 20:40:56 +0200

    Update PREDICTING TIME DELIVERY- TASK 3 NTT.py

--- PREDICTING TIME DELIVERY- TASK 3 NTT.py
+++ PREDICTING TIME DELIVERY- TASK 3 NTT.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[47]:
-
+"""
+Machine Learning techniques to increase profitability. 
+NTTData x Luiss
+Martina Manno, Martina Crisafulli, Olimpia Sannucci, Hanna Carucci Viterbi, Tomas Ryen
+"""
 
 # Import libraries
 import pandas as pd
@@ -23,27 +23,15 @@ from sklearn.svm import SVR
 import time
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-
-# In[25]:
-
-
 # Load the dataset used for the rfm and marketing analysis
 sel_df1 = pd.read_csv("data__mktingstrategy.csv",sep = ",")
 sel_df1 = sel_df1.drop(columns = ['Unnamed: 0'])
-
-
-# In[26]:
-
 
 # transform dates into datetime object
 sel_df1.ts_order_delivered_customer = pd.to_datetime(sel_df1.ts_order_delivered_customer)
 sel_df1.ts_order_delivered_carrier = pd.to_datetime(sel_df1.ts_order_delivered_carrier)
 sel_df1.ts_order_estimated_delivery = pd.to_datetime(sel_df1.ts_order_estimated_delivery)
 sel_df1.ts_order_approved = pd.to_datetime(sel_df1.ts_order_approved)
-
-
-# In[27]:
-
 
 # Add product volume, orders per day and difference between delivery and order approval as additional features
 sel_df1['product_volume_cm3'] = sel_df1.product_length_cm * sel_df1.product_height_cm * sel_df1.product_width_cm
@@ -52,27 +40,15 @@ trend= sel_df1.groupby(["hour_purch","order_id"], as_index=False)["order_id"].si
 trend= trend.groupby("hour_purch").hour_purch.value_counts().to_frame("Order per day")
 trend= trend.reset_index(level=1, drop=True)
 
-
-# In[28]:
-
-
 # Merging and Removal of dataset features
 sel_df1= pd.merge(trend,sel_df1, on="hour_purch")
 sel_df1=sel_df1.drop(['order_status','payment_installments_quantity','order_item_sequence_id'], axis=1, errors='ignore')
 sel_df1=sel_df1.drop(['payment_method_sequence_id'], axis=1, errors='ignore')
 sel_df1=sel_df1.drop(['transaction_value'], axis=1, errors='ignore')
 
-
-# In[29]:
-
-
 # Find and Remove outliers
 sel_df1.boxplot()
 plt.xticks(rotation = 45)
-
-
-# In[30]:
-
 
 # Since there are many outliers, this function remove them 
 def remove_outlier(df_in, col_name):
@@ -84,48 +60,26 @@ def remove_outlier(df_in, col_name):
     df_out = df_in.loc[(df_in[col_name] > fence_low) & (df_in[col_name] < fence_high)]
     return df_out
 
-
-# In[31]:
-
-
 # Remove outliers in the variables having the highest number of them
 sel_df1= remove_outlier(sel_df1, "product_weight_gr")
 sel_df1= remove_outlier(sel_df1, "product_volume_cm3")
 sel_df1= remove_outlier(sel_df1, "price")
 
-
-# In[32]:
-
-
 # Shipping cost into float
 sel_df1["shipping_cost"]  = [float(str(i).replace(",", ".")) for i in sel_df1["shipping_cost"] ]
 
-
-# # Baseline
-
+# Baseline
 # Before applying the models, set a baseline to compare results and verify the funcitoning of the new model, in this case the baseline will be the current delay in time shipping predictions.
-# 
-
-# In[33]:
-
 
 # Extract the day of the estimated delivery and actual delivery
 sel_df1["estimate_day"]=sel_df1.ts_order_estimated_delivery.dt.day
 sel_df1["delivery_day"]= sel_df1.ts_order_delivered_customer.dt.day
-
-
-# In[35]:
-
 
 # Compute the errors of the current data and use them as baseline model
 baseline_data= {}
 baseline_data["Test_RMSE"] = sqrt(mean_squared_error(sel_df1.delivery_day,sel_df1.estimate_day))
 baseline_data["MAE"]= mean_absolute_error(sel_df1.delivery_day, sel_df1.estimate_day)
 baseline_data
-
-
-# In[37]:
-
 
 # Correlation plot of the variables
 mask = np.zeros_like(sel_df1[sel_df1.columns[::-1]].corr(), dtype=np.bool)
@@ -134,12 +88,7 @@ plt.figure(figsize=(10, 6))
 sns.heatmap(sel_df1[sel_df1.columns[::-1]].corr(), cmap=sns.diverging_palette(20, 220, n=200), annot=True, mask=mask, center = 0, )
 plt.title("Heatmap of all the Features", fontsize = 30)
 
-
-# # y as difference between delivery and approval
-
-# In[38]:
-
-
+# y as difference between delivery and approval
 # Define target and feature variables, here the initial features 
 #were chosen based on the possible influence on shipping
 target = 'delivery_approved'
@@ -154,24 +103,12 @@ features = ['product_volume_cm3',
             'Order per day'
            ]
 
-
-# In[40]:
-
-
 # Extract target and features
 X = sel_df1[features]
 y = sel_df1[target]
 
-
-# In[45]:
-
-
 X_data = StandardScaler().fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X_data, y, test_size=0.2, random_state = 14)
-
-
-# In[48]:
-
 
 # we fit the model and compute feature importance to detect the most relevant variables to put in the model
 features = X.columns
@@ -187,10 +124,6 @@ plt.barh(range(len(indices)), importances[indices], color='g', align='center')
 plt.yticks(range(len(indices)), [features[i] for i in indices])
 plt.xlabel('Relative Importance')
 
-
-# In[49]:
-
-
 # Based on the results, two variables are removed: "week_day" and "product_photo_quantity"
 target = 'delivery_approved'
 features = ['product_volume_cm3', 
@@ -202,12 +135,7 @@ features = ['product_volume_cm3',
             'Order per day'
           ]
 
-
-# In[50]:
-
-
 # Applying the models to the data 
-
 models = [
            ['Lasso: ', Lasso()],
            ['Ridge: ', Ridge()],
@@ -231,26 +159,13 @@ for name,curr_model in models :
     curr_model_data["MAE"] = mean_absolute_error(y_test, curr_model.predict(X_test))
     model_data.append(curr_model_data)
 
-
-# In[51]:
-
-
 # See the results, the lowest MAE corresponds to the ExtraTreeRegressor algorithm 
 # and means that the mean delay has been reduced to 4 days 
 model_scores = pd.DataFrame(model_data)
 model_scores
 
-
-# In[52]:
-
-
 # Plotting RMSE results for all models
 model_scores.plot(x = "Name", y = ['Test_RMSE'], kind = "bar" , title = 'RMSE Results' , figsize = (10,8))
 
-
-# In[53]:
-
-
 # Plotting MAE results for all models
 model_scores.plot(x="Name", y=['MAE'], kind="bar" , title = 'MAE Results' , figsize= (10,8))
-
